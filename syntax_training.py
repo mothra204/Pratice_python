53d0a945fc723561631de5b6329263b91c88ff47
Mothra <th.nam204@gmail.com>
2021-11-15 23:52:49 +0700

    update list

--- syntax_training.py
+++ syntax_training.py
@@ -11,8 +11,8 @@
 # print(c.split())
 
 #.format
-a = 'this {} '.format('is')
-print(a)
+# a = 'this {} '.format('is')
+# print(a)
 # a = 'this {a} {b} {c}'.format(a = 'is' , b ='a', c= 'format')
 # print(a)
 
@@ -20,7 +20,19 @@ print(a)
 # b = 0.129454645835634
 # c =  'number = {:1.3f}'.format(b)
 # print(c)
+
 ################ f format
 # name = 'tien'
 # c = f'her name is {name}'
 # print(c)
+
+############## list 
+name  = ["nam",7,90]
+name.append('lol')
+a = name.pop(0)
+# can't contain string and int  only str or int same type
+b = name.sort()
+print(name)
+print(a)
+print(b)
+
