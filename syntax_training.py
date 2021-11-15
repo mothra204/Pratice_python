693b4b856c4a8ee8284f0a2b340c57385ef43708
Mothra <th.nam204@gmail.com>
2021-11-15 23:29:14 +0700

    first push

---
+++ syntax_training.py
@@ -0,0 +1,26 @@
+# #String
+# a = "this is  a strings"
+# b=len('this is not a string dau dcm m')
+# print(a)
+# print(b)
+# print(a[5:])
+
+
+# # function
+# c = 'Hello World'
+# print(c.split())
+
+#.format
+a = 'this {} '.format('is')
+print(a)
+# a = 'this {a} {b} {c}'.format(a = 'is' , b ='a', c= 'format')
+# print(a)
+
+################ value:width.percisionf
+# b = 0.129454645835634
+# c =  'number = {:1.3f}'.format(b)
+# print(c)
+################ f format
+# name = 'tien'
+# c = f'her name is {name}'
+# print(c)
